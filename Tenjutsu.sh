18c71809ace420d7db1042b612f629643c5df273
Jeod <47716344+JeodC@users.noreply.github.com>
2025-04-22 14:45:55 -0400

    Update for westonpack usage

--- Tenjutsu.sh
+++ Tenjutsu.sh
@@ -29,10 +29,42 @@ cd $GAMEDIR
 export LD_LIBRARY_PATH="$GAMEDIR/box64/x64:$GAMEDIR/hashlink:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
+# Mount Weston runtime
+weston_dir=/tmp/weston
+$ESUDO mkdir -p "${weston_dir}"
+weston_runtime="weston_pkg_0.2"
+if [ ! -f "$controlfolder/libs/${weston_runtime}.squashfs" ]; then
+  if [ ! -f "$controlfolder/harbourmaster" ]; then
+    pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
+    sleep 5
+    exit 1
+  fi
+  $ESUDO $controlfolder/harbourmaster --quiet --no-check runtime_check "${weston_runtime}.squashfs"
+fi
+if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+    $ESUDO umount "${weston_dir}"
+fi
+$ESUDO mount "$controlfolder/libs/${weston_runtime}.squashfs" "${weston_dir}"
+
+# rocknix mode on rocknix panfrost; libmali not supported
+if [[ "$CFW_NAME" = "ROCKNIX" ]]; then
+  export rocknix_mode=1
+fi
+
 # Run it
 $GPTOKEYB "$HASHLINK" xbox360 & 
 pm_platform_helper "$HASHLINK" > /dev/null
+
+$ESUDO env \
+BOX64_LD_LIBRARY_PATH="$GAMEDIR/hashlink:$GAMEDIR/hashlink/lib:$LD_LIBRARY_PATH" \
+WRAPPED_PRELOAD="$GAMEDIR/libs.${DEVICE_ARCH}/libSDL2-2.0.so.0" WRAPPED_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}/:$LD_LIBRARY_PATH" \
+BOX64_LD_LIBRARY_PATH="$GAMEDIR/box64/x64" \
+$weston_dir/westonwrap.sh headless noop kiosk crusty_glx_core4es \
 "$BOX64" "$HASHLINK" client.hl
 
 # Cleanup
+$ESUDO $weston_dir/westonwrap.sh cleanup
+if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+    $ESUDO umount "${weston_dir}"
+fi
 pm_finish
